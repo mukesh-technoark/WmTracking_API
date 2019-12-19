5f0c3e07a2ae46fb106aec6555e2028acf734ca9
mohammad <mohammed@technoark.in>
2019-12-19 18:55:02 +0530

    initial committed

--- src/main/java/com/wmtrucking/entity/MaJobTransaction.java
+++ src/main/java/com/wmtrucking/entity/MaJobTransaction.java
@@ -5,10 +5,8 @@
  */
 package com.wmtrucking.entity;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Basic;
@@ -25,7 +23,6 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
